6f3b6845dd62bfe181c517af412d1a7adad0627b
rootewill8 <61087014+rootewill8@users.noreply.github.com>
2020-06-24 09:58:46 -0500

    Update simple_company_schema.sql
    Fixed last_name in script

--- simple_company_schema.sql
+++ simple_company_schema.sql
@@ -33,7 +33,7 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `simple_company`.`CUSTOMER` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `first_name` VARCHAR(45) NULL,
-  `lastName` VARCHAR(45) NULL,
+  `last_name` VARCHAR(45) NULL,
   `gender` CHAR(1) NULL,
   `dob` DATE NULL,
   `email` VARCHAR(45) NULL,
