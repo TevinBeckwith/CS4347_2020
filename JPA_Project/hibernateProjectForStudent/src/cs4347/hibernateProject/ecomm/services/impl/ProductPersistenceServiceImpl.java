9057381f4c63fcdd8ca144607b66cead602c62c9
rootewill8 <61087014+rootewill8@users.noreply.github.com>
2020-07-15 12:00:07 -0500

    Completed Product Service Implementation
    The Product Service Implementation has been completed with no errors/warnings other than a couple of unused imports.

--- JPA_Project/hibernateProjectForStudent/src/cs4347/hibernateProject/ecomm/services/impl/ProductPersistenceServiceImpl.java
+++ JPA_Project/hibernateProjectForStudent/src/cs4347/hibernateProject/ecomm/services/impl/ProductPersistenceServiceImpl.java
@@ -12,14 +12,18 @@
 package cs4347.hibernateProject.ecomm.services.impl;
 
 import java.sql.SQLException;
+import java.util.Collections;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
+import cs4347.hibernateProject.ecomm.entity.Customer;
 import cs4347.hibernateProject.ecomm.entity.Product;
 import cs4347.hibernateProject.ecomm.services.ProductPersistenceService;
 import cs4347.hibernateProject.ecomm.util.DAOException;
+
 
 public class ProductPersistenceServiceImpl implements ProductPersistenceService
 {
@@ -33,34 +37,94 @@ public class ProductPersistenceServiceImpl implements ProductPersistenceService
 	@Override
 	public void create(Product product) throws SQLException, DAOException
 	{
+		try {
+			em.getTransaction().begin();
+			em.persist(product);
+			em.getTransaction().commit();
+		}
+		catch (Exception ex) {
+			em.getTransaction().rollback();
+			throw ex;
+		}
 	}
 
 	@Override
 	public Product retrieve(Long id) throws SQLException, DAOException
 	{
-		return null;
+		try {
+			em.getTransaction().begin();
+			Product product = em.find(Product.class, id);
+			em.getTransaction().commit();
+			return product;
+		}
+		catch (Exception ex) {
+			em.getTransaction().rollback();
+			throw ex;
+		}
 	}
 
 	@Override
 	public void update(Product product) throws SQLException, DAOException
 	{
+		try {
+			em.getTransaction().begin();
+			Product prod = em.find(Product.class, product.getId());
+			prod.setProdName(product.getProdName());
+			prod.setProdDescription(product.getProdDescription());
+			prod.setProdCategory(product.getProdCategory());
+			prod.setProdUPC(product.getProdUPC());
+			em.getTransaction().commit();
+		}
+		catch (Exception ex) {
+			em.getTransaction().rollback();
+			throw ex;
+		}
 	}
 
 	@Override
 	public void delete(Long id) throws SQLException, DAOException
 	{
+		try {	
+			em.getTransaction().begin();
+			Product product = em.find(Product.class, id);
+			em.remove(product);
+			em.getTransaction().commit();
+		}
+		catch (Exception ex) {
+			em.getTransaction().rollback();
+			throw ex;
+		}
 	}
 
 	@Override
-	public Product retrieveByUPC(String upc) throws SQLException, DAOException
+	public Product retrieveByUPC(String prodUPC) throws SQLException, DAOException
 	{
-		return null;
+		try {
+			TypedQuery<Product> q = em.createQuery(("SELECT p FROM Product p WHERE prodUPC = " + prodUPC)
+					, Product.class);
+			Product product = q.getSingleResult();
+			return product;
+		}
+		catch (Exception ex) {
+			em.getTransaction().rollback();
+			throw ex;
+		}
 	}
 
 	@Override
-	public List<Product> retrieveByCategory(int category) throws SQLException, DAOException
+	public List<Product> retrieveByCategory(int prodCategory) throws SQLException, DAOException
 	{
-		return null;
+		try {
+			TypedQuery<Product> q = em.createQuery(("SELECT p FROM Product p WHERE prodCategory = " + Integer.toString(prodCategory))
+					, Product.class);
+			List<Product> products = q.getResultList();
+			return products;
+		}
+		catch (Exception ex) {
+			em.getTransaction().rollback();
+			throw ex;
+		}
 	}
+		
 
 }
